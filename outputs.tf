30507d78cd9b57602db4e9a489261b15d6ba74be
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:55 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "stream_analytics_function_javascript_udfs" {
-  description = "All stream_analytics_function_javascript_udf resources"
-  value       = azurerm_stream_analytics_function_javascript_udf.stream_analytics_function_javascript_udfs
-}
 output "stream_analytics_function_javascript_udfs_input" {
-  description = "List of input values across all stream_analytics_function_javascript_udfs"
-  value       = [for k, v in azurerm_stream_analytics_function_javascript_udf.stream_analytics_function_javascript_udfs : v.input]
+  description = "Map of input values across all stream_analytics_function_javascript_udfs, keyed the same as var.stream_analytics_function_javascript_udfs"
+  value       = { for k, v in azurerm_stream_analytics_function_javascript_udf.stream_analytics_function_javascript_udfs : k => v.input }
 }
 output "stream_analytics_function_javascript_udfs_name" {
-  description = "List of name values across all stream_analytics_function_javascript_udfs"
-  value       = [for k, v in azurerm_stream_analytics_function_javascript_udf.stream_analytics_function_javascript_udfs : v.name]
+  description = "Map of name values across all stream_analytics_function_javascript_udfs, keyed the same as var.stream_analytics_function_javascript_udfs"
+  value       = { for k, v in azurerm_stream_analytics_function_javascript_udf.stream_analytics_function_javascript_udfs : k => v.name }
 }
 output "stream_analytics_function_javascript_udfs_output" {
-  description = "List of output values across all stream_analytics_function_javascript_udfs"
-  value       = [for k, v in azurerm_stream_analytics_function_javascript_udf.stream_analytics_function_javascript_udfs : v.output]
+  description = "Map of output values across all stream_analytics_function_javascript_udfs, keyed the same as var.stream_analytics_function_javascript_udfs"
+  value       = { for k, v in azurerm_stream_analytics_function_javascript_udf.stream_analytics_function_javascript_udfs : k => v.output }
 }
 output "stream_analytics_function_javascript_udfs_resource_group_name" {
-  description = "List of resource_group_name values across all stream_analytics_function_javascript_udfs"
-  value       = [for k, v in azurerm_stream_analytics_function_javascript_udf.stream_analytics_function_javascript_udfs : v.resource_group_name]
+  description = "Map of resource_group_name values across all stream_analytics_function_javascript_udfs, keyed the same as var.stream_analytics_function_javascript_udfs"
+  value       = { for k, v in azurerm_stream_analytics_function_javascript_udf.stream_analytics_function_javascript_udfs : k => v.resource_group_name }
 }
 output "stream_analytics_function_javascript_udfs_script" {
-  description = "List of script values across all stream_analytics_function_javascript_udfs"
-  value       = [for k, v in azurerm_stream_analytics_function_javascript_udf.stream_analytics_function_javascript_udfs : v.script]
+  description = "Map of script values across all stream_analytics_function_javascript_udfs, keyed the same as var.stream_analytics_function_javascript_udfs"
+  value       = { for k, v in azurerm_stream_analytics_function_javascript_udf.stream_analytics_function_javascript_udfs : k => v.script }
 }
 output "stream_analytics_function_javascript_udfs_stream_analytics_job_name" {
-  description = "List of stream_analytics_job_name values across all stream_analytics_function_javascript_udfs"
-  value       = [for k, v in azurerm_stream_analytics_function_javascript_udf.stream_analytics_function_javascript_udfs : v.stream_analytics_job_name]
+  description = "Map of stream_analytics_job_name values across all stream_analytics_function_javascript_udfs, keyed the same as var.stream_analytics_function_javascript_udfs"
+  value       = { for k, v in azurerm_stream_analytics_function_javascript_udf.stream_analytics_function_javascript_udfs : k => v.stream_analytics_job_name }
 }
 
